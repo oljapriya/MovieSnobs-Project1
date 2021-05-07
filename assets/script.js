ec0ab0e2dd50116dae3f9d20b531a97cbe39da4e
oljapriya <poljako@gmail.com>
2021-05-07 11:25:30 -0400

    added url to and working search button

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  var apiKey = "e5c560a7"
+  $("#movieForm").submit(function(event){
+    event.preventDefault()
+
+    var movie = $("#movie").val()
+
+    var result=""
+
+    var url ="http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey
+
+    $.ajax({
+      method:'GET',
+      url:url + "&t=" + movie,
+      success:function(data){
+        console.log(data)
+
+        result = `<img style="float:left" class="img-thumnail" width="200" height="200" src="${data.Poster}"/>`
+          
+        $("#result").html(result)
+
+      }
+    })
+
+  })
+})
