5784dc09d3c780baa2f32d6ece80dac128b723a2
oljapriya <poljako@gmail.com>
2021-05-09 18:34:41 -0400

    fixed reset button added title, genre, plot

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     event.preventDefault()
 
     var movie = $("#movie").val()
-
     var result = ""
+    
 
     var url = "http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey
 
@@ -15,11 +15,22 @@ $(document).ready(function () {
       success: function (data) {
         console.log(data)
 
-        result = `<img style="float:left" class="img-thumnail" width="200" height="200" src="${data.Poster}"/>`
+        result += `
+        <img src="${data.Poster}"/>
+        <div class="resultStyle">
+          <h5><b>TITLE</b>: ${data.Title}</h5>
+          <h5><b>GENRE</b>: ${data.Genre}</h5>
+          <h5><b>PLOT</b>: ${data.Plot}</h5>
+          <h5><b>RATING</b>: ${data.imdbRating}</h5>
+        </div> 
+        `
+        // result = `<img style="float:left" class="img-thumnail" width="200" height="200" src="${data.Poster}"/>`
+        
+        
+         
 
 
-
-
+        
         $("#result").html(result)
        
       }
