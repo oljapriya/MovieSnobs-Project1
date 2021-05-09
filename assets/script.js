d1465ea0a4379744269f45761b6e905293377273
Stephen Moore <stephenmoore33@outlook.com>
2021-05-09 11:27:31 -0400

    added api call for tmdb

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,58 @@
 
-$(document).ready(function(){
-  var apiKey = "e5c560a7"
-  $("#movieForm").submit(function(event){
-    event.preventDefault()
+$(document).ready(function () {
+    var apiKey = "e5c560a7"
+    $("#movieForm").submit(function (event) {
+        event.preventDefault()
 
-    var movie = $("#movie").val()
+        var movie = $("#movie").val()
 
-    var result=""
+        var result = ""
 
-    var url ="http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey
+        var url = "http://www.omdbapi.com/?i=tt3896198&apikey=" + apiKey
 
-    $.ajax({
-      method:'GET',
-      url:url + "&t=" + movie,
-      success:function(data){
-        console.log(data)
+        $.ajax({
+            method: 'GET',
+            url: url + "&t=" + movie,
+            success: function (data) {
+                console.log(data)
 
-        result = `<img style="float:left" class="img-thumnail" src="${data.Poster}"/>`
-          
-        $("#result").html(result)
+                result = `<img style="float:left" class="img-thumnail" src="${data.Poster}"/>`
 
-      }
+                $("#result").html(result)
+
+            }
+        })
+
+    // tmdb api
+        var tmdbAPIKey = "ed33f94e93475b200ae27f0ac9bb479a";
+        var urlSearch = "&query="; // + movie name (#movie)
+        var baseUrl = 'https://api.themoviedb.org/3/search/movie?api_key=';
+
+        var movie = $("#movie").val();
+        var result = "";
+        var url = baseUrl + tmdbAPIKey + urlSearch;
+
+        $.ajax({
+            method: 'GET',
+            url: url + movie,
+            success: function (response) {
+                console.log(response)
+                result = `<h1>${Math.floor(response.results[0].popularity)}</h1>` //rounds popularity to nearest whole number
+                // $("#classforrating").html(result)
+            }
+        })
+
     })
-
-  })
 })
 
 // section for rest button to clear form //
 let btnClear = document.querySelector('#clearbtn')
-let inputs = document.querySelector ('#search')
+let inputs = document.querySelector('#search')
 
 btnClear.addEventListener('click', () => {
-    inputs.forEach(input =>  input.value = '');
+    inputs.forEach(input => input.value = '');
+
+
+
+
 });
