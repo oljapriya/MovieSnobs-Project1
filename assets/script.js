bdc57302ab51bea039809fbe22e678bfce901b6c
Stephen Moore <stephenmoore33@outlook.com>
2021-05-10 20:07:33 -0400

    updated tmdb api to obtain  image oppose to reviews

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
             success: function (data) {
                 console.log(data)
 
-                result = `<img style="float:left" class="img-thumnail" src="${data.Poster}"/>`
+                // result = `<img style="float:left" class="img-thumnail" src="${data.Poster}"/>`
 
                 $("#result").html(result)
 
@@ -27,6 +27,7 @@ $(document).ready(function () {
         var tmdbAPIKey = "ed33f94e93475b200ae27f0ac9bb479a";
         var urlSearch = "&query="; // + movie name (#movie)
         var baseUrl = 'https://api.themoviedb.org/3/search/movie?api_key=';
+        var posterPath = 'https://image.tmdb.org/t/p/w500/' ; // add poster file after
 
         var movie = $("#movie").val();
         var result = "";
@@ -37,8 +38,9 @@ $(document).ready(function () {
             url: url + movie,
             success: function (response) {
                 console.log(response)
-                result = `<h1>${Math.floor(response.results[0].popularity)}</h1>` //rounds popularity to nearest whole number
-                // $("#classforrating").html(result)
+                const fullImgPath = posterPath ; 
+                result = `<img class="img-thumbnail" src="${fullImgPath}${response.results[0].poster_path}"/>` // url and html tag for image
+                $("#result").html(response) // result is class for image
             }
         })
 
